40f494f465b60a9d90877e22ded899b07e5f2136
Yuri Shust <shustts@yandex.by>
2021-03-25 22:41:09 +0300

    Lab 9

---
+++ Lab_9/Lab_9/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab_9
+{
+    class Car : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty;
+        public static readonly DependencyProperty PriceProperty;
+
+        static Car()
+        {
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Car));
+
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Car), metadata,
+                new ValidateValueCallback(ValidateValue));
+        }
+
+        private static object CorrectValue(DependencyObject d, object baseValue)
+        {
+            int currentValue = (int)baseValue;
+            if (currentValue > 1000)  
+                return 1000;
+            return currentValue; 
+        }
+
+        private static bool ValidateValue(object value)
+        {
+            int currentValue = (int)value;
+            if (currentValue >= 0) 
+                return true;
+            return false;
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+    }
+}
---
+++ Lab_9/Lab_9/Control.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_9
+{
+    /// <summary>
+    /// Логика взаимодействия для Control.xaml
+    /// </summary>
+    public partial class Control : UserControl
+    {
+        public Control()
+        {
+            InitializeComponent();
+        }
+        private void Control_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBlock1.Text = textBlock1.Text + "sender: " + sender.ToString() + "\n";
+            textBlock1.Text = textBlock1.Text + "source: " + e.Source.ToString() + "\n\n";
+        }
+    }
+}
