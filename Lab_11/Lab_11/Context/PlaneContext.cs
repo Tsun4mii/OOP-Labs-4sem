ee519c4e25e4ed0dec43e73af1ab4d1a1816dbb9
Yuri Shust <shustts@yandex.by>
2021-04-01 12:12:58 +0300

    Lab 11

---
+++ Lab_11/Lab_11/Context/PlaneContext.cs
@@ -0,0 +1,17 @@
+﻿using Lab_11.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11.Context
+{
+    public class PlaneContext : DbContext
+    {
+        public PlaneContext() : base("DBConnection") { }
+
+        public DbSet<Plane> Plane { get; set; }
+    }
+}
---
+++ Lab_11/Lab_11/Context/WorkerContext.cs
@@ -0,0 +1,17 @@
+﻿using Lab_11.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11.Context
+{
+    public class WorkerContext : DbContext
+    {
+        public WorkerContext() : base("DBConnection") { }
+
+        public DbSet<Worker> Worker { get; set;}
+    }
+}
